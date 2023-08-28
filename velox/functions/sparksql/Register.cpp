f8fe9c25be6f2bbce46169e8762a5bab0b565655
rui-mo <rui.mo@intel.com>
2023-08-28 09:11:50 +0800

    Add SparkSql function make_decimal_by_unscaled_value (5441)

--- velox/functions/sparksql/Register.cpp
+++ velox/functions/sparksql/Register.cpp
@@ -26,6 +26,7 @@
 #include "velox/functions/sparksql/Bitwise.h"
 #include "velox/functions/sparksql/CompareFunctionsNullSafe.h"
 #include "velox/functions/sparksql/DateTimeFunctions.h"
+#include "velox/functions/sparksql/DecimalVectorFunctions.h"
 #include "velox/functions/sparksql/Hash.h"
 #include "velox/functions/sparksql/In.h"
 #include "velox/functions/sparksql/LeastGreatest.h"
@@ -69,6 +70,13 @@ static void workAroundRegistrationMacro(const std::string& prefix) {
 }
 
 namespace sparksql {
+
+void registerExpressionGeneralFunctions(const std::string& prefix) {
+  exec::registerStatefulVectorFunction(
+      prefix + "make_decimal_by_unscaled_value",
+      makeDecimalByUnscaledValueSignatures(),
+      makeMakeDecimalByUnscaledValue);
+}
 
 void registerFunctions(const std::string& prefix) {
   registerFunction<RandFunction, double>({prefix + "rand"});
@@ -228,6 +236,8 @@ void registerFunctions(const std::string& prefix) {
   // Register bloom filter function
   registerFunction<BloomFilterMightContainFunction, bool, Varbinary, int64_t>(
       {prefix + "might_contain"});
+
+  registerExpressionGeneralFunctions(prefix);
 }
 
 } // namespace sparksql
