f1333afb113c8b7fd11074e563a90737ec7a7ee5
Jia Ke <ke.a.jia@intel.com>
2024-11-14 21:43:26 +0800

    Revert "Support jvm version libhdfs in velox (#9835)"
    This reverts commit 10cdf6fd8ca372b11779431ef77a177551c01700.

--- velox/connectors/hive/storage_adapters/hdfs/HdfsFileSystem.cpp
+++ velox/connectors/hive/storage_adapters/hdfs/HdfsFileSystem.cpp
@@ -14,11 +14,11 @@
  * limitations under the License.
  */
 #include "velox/connectors/hive/storage_adapters/hdfs/HdfsFileSystem.h"
+#include <hdfs/hdfs.h>
 #include <mutex>
 #include "velox/common/config/Config.h"
 #include "velox/connectors/hive/storage_adapters/hdfs/HdfsReadFile.h"
 #include "velox/connectors/hive/storage_adapters/hdfs/HdfsWriteFile.h"
-#include "velox/external/hdfs/ArrowHdfsInternal.h"
 
 namespace facebook::velox::filesystems {
 std::string_view HdfsFileSystem::kScheme("hdfs://");
@@ -29,27 +29,21 @@ class HdfsFileSystem::Impl {
   explicit Impl(
       const config::ConfigBase* config,
       const HdfsServiceEndpoint& endpoint) {
-    auto status = filesystems::arrow::io::internal::ConnectLibHdfs(&driver_);
-    if (!status.ok()) {
-      LOG(ERROR) << "ConnectLibHdfs failed ";
-    }
-
-    // connect to HDFS with the builder object
-    hdfsBuilder* builder = driver_->NewBuilder();
-    driver_->BuilderSetNameNode(builder, endpoint.host.c_str());
-    driver_->BuilderSetNameNodePort(builder, atoi(endpoint.port.data()));
-    driver_->BuilderSetForceNewInstance(builder);
-    hdfsClient_ = driver_->BuilderConnect(builder);
+    auto builder = hdfsNewBuilder();
+    hdfsBuilderSetNameNode(builder, endpoint.host.c_str());
+    hdfsBuilderSetNameNodePort(builder, atoi(endpoint.port.data()));
+    hdfsClient_ = hdfsBuilderConnect(builder);
+    hdfsFreeBuilder(builder);
     VELOX_CHECK_NOT_NULL(
         hdfsClient_,
         "Unable to connect to HDFS: {}, got error: {}.",
         endpoint.identity(),
-        driver_->GetLastExceptionRootCause());
+        hdfsGetLastError());
   }
 
   ~Impl() {
     LOG(INFO) << "Disconnecting HDFS file system";
-    int disconnectResult = driver_->Disconnect(hdfsClient_);
+    int disconnectResult = hdfsDisconnect(hdfsClient_);
     if (disconnectResult != 0) {
       LOG(WARNING) << "hdfs disconnect failure in HdfsReadFile close: "
                    << errno;
@@ -60,13 +54,8 @@ class HdfsFileSystem::Impl {
     return hdfsClient_;
   }
 
-  filesystems::arrow::io::internal::LibHdfsShim* hdfsShim() {
-    return driver_;
-  }
-
  private:
   hdfsFS hdfsClient_;
-  filesystems::arrow::io::internal::LibHdfsShim* driver_;
 };
 
 HdfsFileSystem::HdfsFileSystem(
@@ -90,15 +79,13 @@ std::unique_ptr<ReadFile> HdfsFileSystem::openFileForRead(
     path.remove_prefix(index);
   }
 
-  return std::make_unique<HdfsReadFile>(
-      impl_->hdfsShim(), impl_->hdfsClient(), path);
+  return std::make_unique<HdfsReadFile>(impl_->hdfsClient(), path);
 }
 
 std::unique_ptr<WriteFile> HdfsFileSystem::openFileForWrite(
     std::string_view path,
     const FileOptions& /*unused*/) {
-  return std::make_unique<HdfsWriteFile>(
-      impl_->hdfsShim(), impl_->hdfsClient(), path);
+  return std::make_unique<HdfsWriteFile>(impl_->hdfsClient(), path);
 }
 
 bool HdfsFileSystem::isHdfsFile(const std::string_view filePath) {
--- velox/connectors/hive/storage_adapters/hdfs/HdfsWriteFile.cpp
+++ velox/connectors/hive/storage_adapters/hdfs/HdfsWriteFile.cpp
@@ -15,24 +15,23 @@
  */
 
 #include "velox/connectors/hive/storage_adapters/hdfs/HdfsWriteFile.h"
-#include "velox/external/hdfs/ArrowHdfsInternal.h"
+#include <hdfs/hdfs.h>
 
 namespace facebook::velox {
 HdfsWriteFile::HdfsWriteFile(
-    filesystems::arrow::io::internal::LibHdfsShim* driver,
     hdfsFS hdfsClient,
     std::string_view path,
     int bufferSize,
     short replication,
     int blockSize)
-    : driver_(driver), hdfsClient_(hdfsClient), filePath_(path) {
+    : hdfsClient_(hdfsClient), filePath_(path) {
   auto pos = filePath_.rfind("/");
   auto parentDir = filePath_.substr(0, pos + 1);
-  if (driver_->Exists(hdfsClient_, parentDir.c_str()) == -1) {
-    driver_->MakeDirectory(hdfsClient_, parentDir.c_str());
+  if (hdfsExists(hdfsClient_, parentDir.c_str()) == -1) {
+    hdfsCreateDirectory(hdfsClient_, parentDir.c_str());
   }
 
-  hdfsFile_ = driver_->OpenFile(
+  hdfsFile_ = hdfsOpenFile(
       hdfsClient_,
       filePath_.c_str(),
       O_WRONLY,
@@ -43,7 +42,7 @@ HdfsWriteFile::HdfsWriteFile(
       hdfsFile_,
       "Failed to open hdfs file: {}, with error: {}",
       filePath_,
-      driver_->GetLastExceptionRootCause());
+      std::string(hdfsGetLastError()));
 }
 
 HdfsWriteFile::~HdfsWriteFile() {
@@ -53,12 +52,12 @@ HdfsWriteFile::~HdfsWriteFile() {
 }
 
 void HdfsWriteFile::close() {
-  int success = driver_->CloseFile(hdfsClient_, hdfsFile_);
+  int success = hdfsCloseFile(hdfsClient_, hdfsFile_);
   VELOX_CHECK_EQ(
       success,
       0,
       "Failed to close hdfs file: {}",
-      driver_->GetLastExceptionRootCause());
+      std::string(hdfsGetLastError()));
   hdfsFile_ = nullptr;
 }
 
@@ -67,9 +66,9 @@ void HdfsWriteFile::flush() {
       hdfsFile_,
       "Cannot flush HDFS file because file handle is null, file path: {}",
       filePath_);
-  int success = driver_->Flush(hdfsClient_, hdfsFile_);
+  int success = hdfsFlush(hdfsClient_, hdfsFile_);
   VELOX_CHECK_EQ(
-      success, 0, "Hdfs flush error: {}", driver_->GetLastExceptionRootCause());
+      success, 0, "Hdfs flush error: {}", std::string(hdfsGetLastError()));
 }
 
 void HdfsWriteFile::append(std::string_view data) {
@@ -80,20 +79,20 @@ void HdfsWriteFile::append(std::string_view data) {
       hdfsFile_,
       "Cannot append to HDFS file because file handle is null, file path: {}",
       filePath_);
-  int64_t totalWrittenBytes = driver_->Write(
-      hdfsClient_, hdfsFile_, std::string(data).c_str(), data.size());
+  int64_t totalWrittenBytes =
+      hdfsWrite(hdfsClient_, hdfsFile_, std::string(data).c_str(), data.size());
   VELOX_CHECK_EQ(
       totalWrittenBytes,
       data.size(),
       "Write failure in HDFSWriteFile::append {}",
-      driver_->GetLastExceptionRootCause());
+      std::string(hdfsGetLastError()));
 }
 
 uint64_t HdfsWriteFile::size() const {
-  auto fileInfo = driver_->GetPathInfo(hdfsClient_, filePath_.c_str());
+  auto fileInfo = hdfsGetPathInfo(hdfsClient_, filePath_.c_str());
   uint64_t size = fileInfo->mSize;
   // should call hdfsFreeFileInfo to avoid memory leak
-  driver_->FreeFileInfo(fileInfo, 1);
+  hdfsFreeFileInfo(fileInfo, 1);
   return size;
 }
 
