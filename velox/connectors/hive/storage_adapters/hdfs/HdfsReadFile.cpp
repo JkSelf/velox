256981224260cee5fb2c267b1a6432f11c5a8176
Jia Ke <ke.a.jia@intel.com>
2024-11-18 21:13:23 +0800

    tmp

--- velox/connectors/hive/storage_adapters/hdfs/HdfsReadFile.cpp
+++ velox/connectors/hive/storage_adapters/hdfs/HdfsReadFile.cpp
@@ -27,9 +27,9 @@ HdfsReadFile::HdfsReadFile(hdfsFS hdfs, const std::string_view path)
     auto errMsg = fmt::format(
         "Unable to get file path info for file: {}. got error: ",
         filePath_);
-    if (std::strstr(errMsg, "FileNotFoundException") != nullptr) {
-      VELOX_FILE_NOT_FOUND_ERROR(errMsg);
-    }
+    // if (std::strstr(errMsg, "FileNotFoundException") != nullptr) {
+    //   VELOX_FILE_NOT_FOUND_ERROR(errMsg);
+    // }
     VELOX_FAIL(errMsg);
   }
 }
