8035aa2a52cb6433f863bdd2dfbce7fc71c9dfe2
Ke Jia <ke.a.jia@intel.com>
2025-04-07 14:08:21 +0000

    bhj optimization

--- velox/exec/HashJoinBridge.h
+++ velox/exec/HashJoinBridge.h
@@ -55,6 +55,12 @@ class HashJoinBridge : public JoinBridge {
       bool hasNullKeys,
       HashJoinTableSpillFunc&& tableSpillFunc);
 
+  void setHashTable(
+      std::shared_ptr<BaseHashTable> table,
+      SpillPartitionSet spillPartitionSet,
+      bool hasNullKeys,
+      HashJoinTableSpillFunc&& tableSpillFunc);
+
   /// Invoked by the probe operator to append the spilled hash table partitions
   /// while probing. The function appends the spilled table partitions into
   /// 'spillPartitionSets_' stack. This only applies if the disk spilling is
